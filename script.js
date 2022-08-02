b00963751a776cc2e84aa0fedac3b8a31d5aeba8
Diksha Jain <89463068+iAmDiksha@users.noreply.github.com>
2022-08-02 13:53:30 +0530

    Files are added

---
+++ script.js
@@ -0,0 +1,27 @@
+let button = document.querySelector('.circle');
+
+button.addEventListener('click',adviceGenerator);
+
+function adviceGenerator()
+{
+    let URL = 'https://api.adviceslip.com/advice';
+
+    fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+        let ID = `ADVICE #${data.slip.id}`;
+        document.querySelector('.advice_id').innerHTML = ID;
+
+        let quote = `<q>${data.slip.advice}</q>`;
+        document.querySelector('.advice').innerHTML = quote;
+    })
+}
+
+
+if(window.innerWidth < 375){
+    let divider = document.querySelector('.divider');
+    divider.src = 'images/pattern-divider-mobile.svg';
+    divider.style.width = '200px';
+}
+
+
